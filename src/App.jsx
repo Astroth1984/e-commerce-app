c81468d2c3ae1fde0e5e262305f627b1379816d2
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-16 18:57:19 +0100

    React Login and Register Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Product from './pages/Product';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 const App = () => {
-  return <Product />;
+  return <Login />;
 };
 
 export default App;
