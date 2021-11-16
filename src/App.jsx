a6df67681cead1b3a1e13414ba6a4bad8aa89c95
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-16 23:50:10 +0100

    React Shopping Cart Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 const App = () => {
-  return <Login />;
+  return <Cart />;
 };
 
 export default App;
