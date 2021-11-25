fe1fdc6da6d88b7d94d128b62c1ced41853545a3
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-25 23:01:19 +0100

    react-router-dom 5.3.0 Router

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,42 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import {
   BrowserRouter as Router,
+  Switch,
   Route,
-  Routes     
+  Redirect,
 } from "react-router-dom";
 import Success from './pages/Success';
+import { useSelector } from "react-redux";
 
 const App = () => {
+
+  const user = useSelector((state) => state.user.currentUser);
+  
+
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/products/:category" element={<ProductList />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/success" element={<Success />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-  </Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
