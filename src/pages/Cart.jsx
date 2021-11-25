ed2a904cb1635613914141dfe936ec6bb74ed5a6
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-25 19:36:13 +0100

    React Stripe Payment Implementation with Stripe Token

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,6 +5,16 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { mobile } from "../responsive";
 import {useSelector} from 'react-redux';
+import StripeCheckout from "react-stripe-checkout";
+import {useState, useEffect} from "react";
+import {userRequest} from "../requestMethods";
+import { useNavigate } from 'react-router-dom';
+
+
+
+//Stripe Public Key
+//const KEY = process.env.REACT_APP_STRIPE
+const KEY = "pk_test_51Jxkh1A9FZFCrrHnaCOl7vb59G06dB29AaDWNXdjoxIVelJQEmyd92LlAlkNmv1DmxOU8zJnNGSU59IBC2SXVHGV00HdQ7b4vV"
 
 const Container = styled.div``;
 
@@ -157,7 +167,28 @@ const Button = styled.button`
 
 const Cart = () => {
 
-  const cart = useSelector(state => state.cart)
+  const cart = useSelector(state => state.cart);
+  const [stripeToken, setStripeToken] = useState(null);
+  const navigate = useNavigate();
+
+  const onToken = (token)=>{
+    setStripeToken(token);
+  };
+  //console.log(stripeToken);
+
+  useEffect(()=>{
+    const makeRequest = async() =>{
+      try{
+        const res = await userRequest.post("/checkout/payment",{
+          tokenId: stripeToken.id,
+          amount: cart.total * 100,
+        });
+        navigate("/success", {data:res.data});
+      }catch{}
+    };
+    stripeToken && makeRequest();
+  },[stripeToken, cart.total, navigate]);
+
   return (
     <Container>
       <Navbar />
@@ -221,7 +252,19 @@ const Cart = () => {
               <SummaryItemText>Total</SummaryItemText>
               <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
             </SummaryItem>
-            <Button>CHECKOUT NOW</Button>
+            <StripeCheckout 
+                    name="Astr-Oth Shop"
+                    image="https://w7.pngwing.com/pngs/209/430/png-transparent-electric-blue-text-brand-apps-electrum-blue-text-logo-thumbnail.png"
+                    shippingAddress
+                    billingAddress={true}
+                    bitcoin={true}
+                    description= {`Your Total is $${cart.total}`}
+                    amount={cart.total * 100}
+                    token ={onToken}
+                    stripeKey={KEY}
+                >
+                    <Button>CHECKOUT NOW</Button>
+            </StripeCheckout>
           </Summary>
         </Bottom>
       </Wrapper>
