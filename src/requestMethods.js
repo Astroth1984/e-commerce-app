709cad1173b386e32ae79a2933e98a1dab74754a
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-26 22:54:26 +0100

    Get JWT Token from Local Storage Authentication

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTgzOWJhMWU1NmI0MmIzM2Q4NTU4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzc2Nzk1MSwiZXhwIjoxNjM4MDI3MTUxfQ.nViZtjNBziwQwF1hdpgYL19LMsnUVNQ-HzS41yACDMc";
+// const TOKEN =
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTgzOWJhMWU1NmI0MmIzM2Q4NTU4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzc2Nzk1MSwiZXhwIjoxNjM4MDI3MTUxfQ.nViZtjNBziwQwF1hdpgYL19LMsnUVNQ-HzS41yACDMc";
+
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
