fa92c4fdbc6c7ce3f733027efb991e814d52e3a5
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-28 13:39:23 +0100

    verify local storage for jwt Token

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN =
+let TOKEN = "";
 //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTgzOWJhMWU1NmI0MmIzM2Q4NTU4OSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzc2Nzk1MSwiZXhwIjoxNjM4MDI3MTUxfQ.nViZtjNBziwQwF1hdpgYL19LMsnUVNQ-HzS41yACDMc";
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+if (localStorage.getItem("persist:root")) {
+    TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken;
+} 
 
+console.log(TOKEN);
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
 });
